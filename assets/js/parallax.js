ef3248d08a6eb43124e96b40239da40527f50770
Ian Channing <ian@ianchanning.com>
2017-01-24 22:36:27 +0100

    Adding in polyfills, not that it's much use cause it's still broken in < IE9, now works in IE9 with ms- transform prefix, adding a title font, prettying up the javascript, simplified the javascript to use just transformY, adding in vertical lines to suggest scrolling

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,25 +1,83 @@
-function goParallax() {
-	window.addEventListener("scroll", function(event){
-		var top = this.pageYOffset;
-		var shift = function(layer) {
-			var speed = layer.getAttribute('data-speed');
-			var yPos = function() {return -(top * speed / 100);};
-			var coords = function(x,y,z) {
-				return [x,y,z].map(function(x) {return x+'px';}).join(',');
-			};
-			var style = function() {
-				return'transform: translate3d('+coords(0,yPos(),0)+')';
-			};
-			layer.setAttribute('style', style());
-		};
-		// document.getElementsByClassName is 'array like' not an actual array
-		Array.prototype.forEach.call(document.getElementsByClassName('parallax'), shift);
+/**
+ * Shift parallax layers based on a y offset
+ * @param  {Number} top y offset
+ */
+function goParallax(top) {
+    "use strict";
 
-	});
+    /**
+     * Generate CSS pixel coordinates
+     * @param  {Array} xs List of pixel numbers
+     * @return {String}   CSV pixel coords e.g. 1px,2px,3px
+     */
+    var coords = function(xs) {
+        return xs.map(function(x) {return x+'px';}).join(',');
+    };
+
+    /**
+     * @deprecated use translateY instead
+     * @param  {Number} x pixels
+     * @param  {Number} y pixels
+     * @param  {Number} z pixels
+     * @return {String}   CSS transform
+     */
+    var translate3d = function(x,y,z) {
+        return 'transform: translate3d('+coords([x,y,z])+');';
+    };
+
+    /**
+     * translateY transform
+     * @param  {Number} y pixels
+     * @return {String}   CSS transform
+     */
+    var translateY = function(y) {
+        return 'transform: translateY('+coords([y])+');';
+    };
+
+    /**
+     * ms prefix
+     * @param  {String} css
+     * @return {String}     css with ms prefix
+     */
+    var ms = function(css) {return 'ms-'+css;};
+
+    /**
+     * Move a layer
+     * @param  {Object} layer Element
+     */
+    var shift = function(layer) {
+        var speed = layer.getAttribute('data-speed');
+        var yPos = -(top * speed / 100);
+        var style = translateY(yPos);
+        layer.setAttribute('style', style+ms(style));
+    };
+
+    // document.getElementsByClassName is 'array like' not an actual array
+    Array.prototype.forEach.call(
+        document.getElementsByClassName('parallax'),
+        shift
+    );
 }
 
 function init() {
-	goParallax();
+    "use strict";
+
+    goParallax(0);
+
+    if (window.addEventListener) {
+        window.addEventListener('scroll', function(){
+            goParallax(this.pageYOffset);
+        });
+        return;
+    }
+    if (window.attachEvent) {
+        window.attachEvent('scroll', function(){
+            goParallax(this.pageYOffset);
+        });
+        return;
+    }
 }
 
-document.body.onload = init();
+document.body.onload = function() {
+    init();
+};
